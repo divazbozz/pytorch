1b088c9af29497e674bd2efe565761aa086b0a1a
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2019-05-21 15:34:23 -0700

    Update on "Move THCTensor_{random, clampedRandom, cappedRandom} to ATen"
    Move THCTensor_{random, clampedRandom, cappedRandom} to ATen
    
    gh-metadata: pytorch pytorch 20620 gh/syed-ahmed/1/head

--- aten/src/ATen/native/cuda/Distributions.cu
+++ aten/src/ATen/native/cuda/Distributions.cu
@@ -468,7 +468,17 @@ void random_kernel_cuda(TensorIterator& iter, uint64_t range, int64_t base, Gene
       distribution_nullary_kernel<scalar_t, uint32_t, curand4_engine_calls>(iter,
         gen,
         [] __device__ (curandStatePhilox4_32_10_t* state) {
-          return curand4(state);
+          uint4 rand_val;
+        #ifndef __HIP_PLATFORM_HCC__
+          rand_val = curand4(state);
+        #else
+          // HIP doesn't have curand4
+          rand_val.x = curand(state);
+          rand_val.y = curand(state);
+          rand_val.z = curand(state);
+          rand_val.w = curand(state);
+        #endif
+          return rand_val;
         },
         random_func);
     }
